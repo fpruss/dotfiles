bf94330b30663a3c09ef97a98ce982e30fd5247f
FPruss <fpruss@mailbox.org>
2022-09-29 09:11:35 +0200

    adding fish and leftwm config files

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias vimdiff="nvim -d"
+alias vim="nvim"
+alias vi="nvim"
+alias config='/usr/bin/git --git-dir=/home/prussf/dotfiles/ --work-tree=/home/prussf'
