5fe6e7012b154110532583d29ac1838089d4c640
Friedemann Pruss <friedemann.pruss@swarco.com>
2025-10-23 17:15:39 +0200

    use fzf-tmux if in tmux session

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,4 +4,12 @@ if status is-interactive
   set -gx PATH $PATH $HOME/.krew/bin
   set -g fish_key_bindings fish_vi_key_bindings
   set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+
+  function fzf --wraps=fzf --description="Use fzf-tmux if in tmux session"
+  if set --query TMUX
+    fzf-tmux $argv
+  else
+    command fzf $argv
+  end
+end
 end
