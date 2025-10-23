20a661c0fb1c9f324063ff1c9194d438b50c7180
Friedemann Pruss <friedemann.pruss@swarco.com>
2025-10-23 13:22:33 +0200

    adding fish vim bindigns

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
   starship init fish | source
   set -gx PATH $PATH $HOME/.krew/bin
+  set -g fish_key_bindings fish_vi_key_bindings
 end
