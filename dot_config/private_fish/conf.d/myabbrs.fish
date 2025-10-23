bdeb9fd57b1f5723cb294234ed595cb7732b7eb2
Friedemann Pruss <friedemann.pruss@swarco.com>
2025-10-23 13:45:48 +0200

    adding git and chezmoi abbreviations

--- dot_config/private_fish/conf.d/myabbrs.fish
+++ dot_config/private_fish/conf.d/myabbrs.fish
@@ -1,2 +1,10 @@
 abbr -a -- k kubectl
 abbr -a -- t talosctl
+abbr -a -- gs 'git status'
+abbr -a -- czms 'chezmoi status'
+abbr -a -- ga 'git add'
+abbr -a -- czma 'chezmoi add'
+abbr -a -- gd 'git diff'
+abbr -a -- czmd 'chezmoi diff'
+abbr -a -- gc 'git commit -m ""'
+abbr -a -- czmcd 'chezmoi cd'
